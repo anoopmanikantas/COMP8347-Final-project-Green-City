3c739f595ea9180173c010ab5acf2d0044485b5f
Arjun Patel <arjun.patel100701@gmail.com>
2024-07-01 15:23:30 -0400

    added urls and created superuser of myname

--- GreenCity/urls.py
+++ GreenCity/urls.py
@@ -1,12 +1,7 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
-from myapp import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('user_profile/', views.user_profile, name='user_profile'),
-    path('building_permit_details/', views.building_permit_details, name='building_permit_details'),
-    path('status_details/', views.status_details, name='status_details'),
-    # Add other paths as needed
-] + staticfiles_urlpatterns()
+    path('admin/', admin.site.urls),
+    path('myapp/', include('myapp.urls')),
+]
---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+from myapp import views
+
+app_name ='myapp'
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('user_profile/', views.user_profile, name='user_profile'),
+    path('building_permit_details/', views.building_permit_details, name='building_permit_details'),
+    path('status_details/', views.status_details, name='status_details'),
+    # Add other paths as needed
+] + staticfiles_urlpatterns()
