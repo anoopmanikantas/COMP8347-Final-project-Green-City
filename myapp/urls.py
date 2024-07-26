7276e0a0589a426d722f4f76c6015ba08eb08a0f
AdityaTohan <tohanaditya@gmail.com>
2024-07-26 13:39:27 -0400

    Password_Reset Configured

--- myapp/urls.py
+++ myapp/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name ="register/password_reset.html"), name='reset_password'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = "register/password_reset_done.html"), name='password_reset_done'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = "register/password_reset_sent.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = "register/password_reset_form.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name = "register/password_reset_done.html"), name='password_reset_complete'),
     path('accounts/login/', views.user_login, name='accounts_login'),
