8827e5e7b1023c502c34d9c9a932f2b5ad858c6a
smita172 <smitakhristi48@gmail.com>
2024-07-26 20:32:38 -0400

    user history tracking on user profile, login signup validation for admin side

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, ContactModel
+from .models import CustomUser, ContactModel, BuildingPermit, UserHistory
 
 # Register your models here.
 # admin.site.register(AdminUser)
 admin.site.register(CustomUser)
 admin.site.register(ContactModel)
+admin.site.register(BuildingPermit)
+admin.site.register(UserHistory)
 
